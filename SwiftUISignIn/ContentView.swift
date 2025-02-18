7bce10051f20b746eeb634c6d5c6b785bc04a6b3
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2025-02-18 15:39:37 +0800

    Initial Commit

---
+++ SwiftUISignIn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISignIn
+//
+//  Created by Lyle Dane Carcedo on 2/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISignIn/SwiftUISignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISignInApp.swift
+//  SwiftUISignIn
+//
+//  Created by Lyle Dane Carcedo on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
