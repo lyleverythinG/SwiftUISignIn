a929245199466207a9396d92083d265a811d9867
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2025-02-18 16:21:04 +0800

    Ignore GoogleService-Info.plist

--- SwiftUISignIn/SwiftUISignInApp.swift
+++ SwiftUISignIn/SwiftUISignInApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftUISignInApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
